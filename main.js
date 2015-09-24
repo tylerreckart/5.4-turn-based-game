05dfda2e4bb50ecbb0f6c36ca0795a393a71bc30
Tyler Reckart <tyler.reckart@gmail.com>
2015-09-24 15:11:32 -0400

    Update damage for random roll

--- main.js
+++ main.js
@@ -1,46 +1,3 @@
-var attack = function(maximumRoll, minimumRoll) {
-  return Math.floor(Math.random() * (maximumRoll - minimumRoll + 1))  + minimumRoll;
-};
-
-function Trainer(name, hp, maximumRoll, minimumRoll){
-  this.name = name;
-  this.hp = hp;
-  this.maximumRoll = maximumRoll;
-  this.minimumRoll = minimumRoll;
-  this.damage = attack(maximumRoll, minimumRoll);
-}
-
-function Pokemon(name, hp, maximumRoll, minimumRoll){
-  this.name = name;
-  this.hp = hp;
-  this.maximumRoll = maximumRoll;
-  this.minimumRoll = minimumRoll;
-  this.damage = attack(maximumRoll, minimumRoll);
-}
-
-Trainer.prototype.attack = function(target) {
-  var currentHp = target.hp;
-  currentHp = currentHp - this.damage;
-  target.hp = currentHp;
-  console.log(this.name + " did " + this.damage + " to " + target.name);
-  target.attack(this);
-
-  if(this.hp < 0){
-    console.log('You have fainted');
-  } else {}
-};
-
-Pokemon.prototype.attack = function(target) {
-  var currentHp = target.hp;
-  currentHp = currentHp - this.damage;
-  target.hp = currentHp;
-  console.log(this.name + " did " + this.damage + " to " + target.name);
-
-  if(this.hp < 0){
-    console.log(this.name + ' has fainted');
-  } else {}
-};
-
 // player
 var Brenden = new Trainer('Brenden', 100, 55, 15);
 
@@ -51,3 +8,58 @@ var Charizard = new Pokemon('Charizard', 100, 50, 20);
 var Alakazam = new Pokemon('Alakazam', 100, 65, 10);
 var Poliwhirl = new Pokemon('Poliwhirl', 100, 40, 35);
 var Mewtwo = new Pokemon('Mewtwo', 100, 80, 25);
+
+var attack = function(maximumRoll, minimumRoll) {
+  return Math.floor(Math.random() * (maximumRoll - minimumRoll + 1))  + minimumRoll;
+};
+
+function Trainer(name, hp, maximumRoll, minimumRoll){
+  this.name = name;
+  this.hp = hp;
+  this.maximumRoll = maximumRoll;
+  this.minimumRoll = minimumRoll;
+//  this.damage = attack(maximumRoll, minimumRoll);
+}
+
+function Pokemon(name, hp, maximumRoll, minimumRoll){
+  this.name = name;
+  this.hp = hp;
+  this.maximumRoll = maximumRoll;
+  this.minimumRoll = minimumRoll;
+//  this.damage = attack(maximumRoll, minimumRoll);
+}
+
+Trainer.prototype.attack = function(target) {
+  var currentHp = target.hp;
+  var damage = attack(this.maximumRoll, this.minimumRoll);
+  currentHp = currentHp - damage;
+  target.hp = currentHp;
+  console.log(this.name + " did " + damage + " to " + target.name);
+
+  target.attack(this);
+
+  if(this.hp <= 0){
+    console.log('You have fainted');
+    location.reload();
+  } else {}
+};
+
+function attackDelay() {
+  return window.setTimeout(returnAttack, 2000);
+}
+
+function returnAttack() {
+  attack(this);
+}
+
+Pokemon.prototype.attack = function(target) {
+  var currentHp = target.hp;
+  var damage = attack(this.maximumRoll, this.minimumRoll);
+  currentHp = currentHp - damage;
+  target.hp = currentHp;
+  console.log(this.name + " did " + damage + " to " + target.name);
+
+  if(this.hp <= 0){
+    console.log(this.name + ' has fainted');
+  } else {}
+};
